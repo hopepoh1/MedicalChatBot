09ceb60233531842e81657e575024ccac7341fc0
hopepoh1 <metinumuttatli@hotmail.com>
2024-03-28 16:44:11 +0300

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'meti-umut-tatli',
+    author_email= 'metinumuttatli@hotmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
