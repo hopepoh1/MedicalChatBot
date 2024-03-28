dc1619d709e22e4c521131454fe7a4393606fa1a
hopepoh1 <metinumuttatli@hotmail.com>
2024-03-28 18:11:31 +0300

    templates added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from src.helper import download_hugging_face_embeddings
 from pinecone import Pinecone
 from langchain_pinecone import PineconeVectorStore
 from langchain.prompts import PromptTemplate
-from langchain.llms import CTransformers
+from langchain_community.llms import CTransformers
 from langchain.chains import RetrievalQA
 from dotenv import load_dotenv
 from src.prompt import *
